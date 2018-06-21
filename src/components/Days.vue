25ef4aa8ad9ad5966e38dbb89d2e5d6556d74c7a
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-06-21 18:34:41 -0400

    added a v-if to give a class to winning team

--- src/components/Days.vue
+++ src/components/Days.vue
@@ -5,10 +5,33 @@
       <div class = "june-days" >June {{days[index]}} <hr></div>
       <span class="matches" v-for="match in round.matches" :key="match.id">
         
-        <div class = "game">
-       <div class = "first-team" >{{match.team1.name}}: <span class = "score">{{match.score1}}</span> </div> <div class = "second-team">{{match.team2.name}}: <span class = "score">{{match.score2}}</span> 
-       </div>
-       </div>
+            <div class="game" v-if="match.score1>match.score2">
+                <div class="first-team bold">{{match.team1.name}}:
+                    <span class="score bold">{{match.score1}}</span>
+                </div>
+                <div class="second-team">{{match.team2.name}}:
+                    <span class="score">{{match.score2}}</span>
+                </div>
+                
+            </div>
+                        <div class="game" v-else-if="match.score1<match.score2">
+                <div class="first-team ">{{match.team1.name}}:
+                    <span class="score">{{match.score1}}</span>
+                </div>
+                <div class="second-team bold">{{match.team2.name}}:
+                    <span class="score bold">{{match.score2}}</span>
+                </div>
+                
+            </div>
+                        <div class="game" v-else>
+                <div class="first-team ">{{match.team1.name}}:
+                    <span class="score">{{match.score1}}</span>
+                </div>
+                <div class="second-team ">{{match.team2.name}}:
+                    <span class="score">{{match.score2}}</span>
+                </div>
+                
+            </div>
        </span>
      </span>
 
@@ -24,9 +47,8 @@ export default {
   data() {
     return {
       rounds: [],
-      days: [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28],
+      days: [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
     };
-   
   },
   mounted() {
     axios
@@ -45,49 +67,45 @@ export default {
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   grid-template-rows: minmax(100px, auto);
   text-align: left;
-  
-  grid-column-gap: 30px;
+
+  grid-column-gap: 20px;
   grid-row-gap: 10px;
-  
+}
 
-  }
-  
-
-.rounds{
- padding:30px 30px 20px 30px;
-
-  box-shadow:10px 10px 20px gray;
+.rounds {
+  padding: 30px 30px 20px 30px;
+  margin: 10px;
+  box-shadow: 10px 10px 20px gray;
   background-color: #f9f9f9;
-
 }
 
-.rounds:nth-of-type(16){
-  display:none;
+.rounds:nth-of-type(16) {
+  display: none;
 }
-.rounds:nth-of-type(17){
-  display:none;
+.rounds:nth-of-type(17) {
+  display: none;
 }
-.rounds:nth-of-type(18){
-  display:none;
+.rounds:nth-of-type(18) {
+  display: none;
 }
-.rounds:nth-of-type(19){
-  display:none;
+.rounds:nth-of-type(19) {
+  display: none;
 }
-.rounds:nth-of-type(20){
-  display:none;
+.rounds:nth-of-type(20) {
+  display: none;
 }
-.game{
+.game {
   margin-bottom: 10px;
 }
-.score{
-  font-weight: 500;
-}
-.june-days{
-  font-size:30px;
-}
-hr{
 
+.june-days {
+  font-size: 30px;
+}
+.bold {
+  font-weight: bold;
+}
 
-    
+.tie-color {
+  color: red;
 }
 </style>
