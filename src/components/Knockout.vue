6bb942011f28b09dac01689c0e59e06e3ef1adf1
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-06-25 19:36:25 -0400

    got a simple setup for the bracket for knockout, needs mad work still

--- src/components/Knockout.vue
+++ src/components/Knockout.vue
@@ -1,111 +1,97 @@
 <template>
      <div class="knockout">
-    <h1>knocout</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
-    <main id="tournament">
-	<ul class="round round-1">
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Lousville <span>79</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">NC A&T <span>48</span></li>
+    <h1>Knockout Stage</h1>
 
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Colo St <span>84</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Missouri <span>72</span></li>
+    <div class="tournament">
+  <!-- tournament logo -->
+  <div class="tournament__logo-container tournament__logo-container--right">
+    <strong class="tournament__logo tournament__logo--gold"></strong>
+  </div>
+  <div class="tournament__grid">
+    <!-- tournament__round 1/8 -->
+    <div class="tournament__round tournament__round--first-round">
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+    </div>
 
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top ">Oklahoma St <span>55</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom winner">Oregon <span>68</span></li>
+    <div class="tournament__round tournament__round--first-round">
+      <div class="tournament__match tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >1 Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+    </div>
 
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Saint Louis <span>64</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">New Mexico St <span>44</span></li>
+    <!-- tournament__round 1/2 -->
+    <div class="tournament__round">
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+    </div>
 
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Memphis <span>54</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">St Mary's <span>52</span></li>
+    <!-- tournament__round final -->
+    <div class="tournament__round tournament__round--final">
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+    </div>
 
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Mich St <span>65</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Valparaiso <span>54</span></li>
+    <!-- tournament__round winner -->
+    <div class="tournament__round tournament__round--winner">
+      <div class="tournament__match">
+        <a class="tournament__match__team" >Lorem ipsum.</a>
+      </div>
+    </div>
+  </div>
 
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Creighton <span>67</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Cincinnati <span>63</span></li>
-
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Duke <span>73</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Albany <span>61</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-2">
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Lousville <span>82</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Colo St <span>56</span></li>
-
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Oregon <span>74</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Saint Louis <span>57</span></li>
-
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top ">Memphis <span>48</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom winner">Mich St <span>70</span></li>
-
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top ">Creighton <span>50</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom winner">Duke <span>66</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-3">
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Lousville <span>77</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Oregon <span>69</span></li>
-
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top ">Mich St <span>61</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom winner">Duke <span>71</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-4">
-		<li class="spacer">&nbsp;</li>
-		
-		<li class="game game-top winner">Lousville <span>85</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Duke <span>63</span></li>
-		
-		<li class="spacer">&nbsp;</li>
-	</ul>		
-</main>
+</div>
   </div>
 </template>
 
@@ -124,61 +110,115 @@ export default {
     display: inline;
      height:100vh;
 }
+
+.bracket{
+    margin-top:80px;
+}
+body {
+  background: #383738;
+}
+.tournament {
+  position: relative;
+  min-height: 300px;
+  padding: 30px 0 0;
+}
+.tournament__grid {
+  font-size: 0;
+  line-height: 0;
+  display: flex;
+  align-items: stretch;
+  min-height: 400px;
+}
+.tournament__round {
+  position: relative;
+  flex: 1 0;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+.tournament__match {
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  flex: 1 0;
+  align-items: center;
+  min-height: 100px;
+}
+.tournament__match:first-child {
+  margin: 0 !important;
+}
+.tournament__round--first-round .tournament__match {
+  padding-left: 0;
+}
+.tournament__round--winner .tournament__match {
+  padding-right: 0;
+  height: 32px;
+}
+.tournament__match:after {
+  content: '';
+  position: absolute;
+  right: 0;
+  width: 2px;
+  background: rgba(0, 0, 0, .2);
+  top: 25%;
+  bottom: 25%;
+}
+.tournament__round--winner .tournament__match:after, .tournament__round--end-point .tournament__match:after {
+  display: none;
+}
+.tournament__match__team {
+  font-size: 14px;
+  font-weight: bold;
+  transition: color 0.3s ease;
+  color: #ccc;
+  text-decoration: none;
+  box-sizing: border-box;
+  background: rgba(0, 0, 0, .2);
+  display: block;
+  position: relative;
+  width: auto;
+  height: 32px;
+  line-height: 32px;
+  padding: 0 36px 0 36px;
+  margin: auto;
+}
+.tournament__match__team:focus, .tournament__match__team:hover {
+  color: #fff;
+}
+.tournament__match__team:before, .tournament__match__team:after {
+  content: '';
+  position: absolute;
+  top: 50%;
+  width: 999px;
+  height: 2px;
+  margin-top: -1px;
+  background: rgba(0, 0, 0, .2);
+}
+.tournament__match__team:after {
+  left: 100%;
+}
+.tournament__round:last-child .tournament__match__team:after, .tournament__round--end-point .tournament__match__team:after {
+  display: none;
+}
+.tournament__match__team:before {
+  right: 100%;
+}
+.tournament__round:first-child .tournament__match__team:before {
+  display: none;
+}
+.tournament__match__team.tournament__match__team--no-prev-round:before {
+  display: none;
+}
+.vhidden {
+  visibility: hidden;
+}
+
+
 /*
  *  Flex Layout Specifics
 */
-main{
-  display:flex;
-  flex-direction:row;
-}
-.round{
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  width:200px;
-  list-style:none;
-  padding:0;
-}
-  .round .spacer{ flex-grow:1; }
-  .round .spacer:first-child,
-  .round .spacer:last-child{ flex-grow:.5; }
 
-  .round .game-spacer{
-    flex-grow:1;
-  }
-
-/*
- *  General Styles
-*/
-body{
-  font-family:sans-serif;
-  font-size:small;
-  padding:10px;
-  line-height:1.4em;
-}
-
-li.game{
-  padding-left:20px;
-}
-
-  li.game.winner{
-    font-weight:bold;
-  }
-  li.game span{
-    float:right;
-    margin-right:5px;
-  }
-
-  li.game-top{ border-bottom:1px solid #aaa; }
-
-  li.game-spacer{ 
-    border-right:1px solid #aaa;
-    min-height:40px;
-  }
-
-  li.game-bottom{ 
-    border-top:1px solid #aaa;
-  }
 
 
 
