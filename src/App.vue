016f928a294da80f577aa4d155ca3894387e249d
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-07-01 13:17:12 -0400

    fixed ux

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class = "title-header"><h1><router-link to="/">World Cup 2018</router-link><br> </h1><h2>  Stages: <router-link to="/group">Group</router-link><span> </span> <router-link to="/knockout">Knockout</router-link></h2></div>
+    <div class = "title-header"><h1><router-link to="/">World Cup 2018</router-link><br> </h1><h2><router-link to="/group">Group</router-link><span> </span> <router-link to="/knockout">Knockout</router-link></h2></div>
 
 
        
@@ -38,18 +38,18 @@ background: linear-gradient(315deg, rgb(252, 0, 170), rgb(0, 219, 222));
 .title-header{
   position:absolute;
   top:5px;
-   background-color: #f9f9f9;
+
   border-radius:10px;
   padding:10px 18px 10px 18px;
   max-width:400px;
   margin:0 auto;
   box-shadow: 5px 5px 20px gray;
-  background: linear-gradient(to right, rgb(69, 127, 202), rgb(86, 145, 200));
+  background: 	#2CA2FC;
   color:white;
   z-index: 999999999999;
 }
 h1{
-  font-size:28px;
+  font-size:32px;
   margin:5px 5px 10px 5px;
   font-weight: bold;
 
@@ -57,14 +57,23 @@ h1{
 h2{
   margin:5px;
   font-weight:bold;
+    margin-top:20px;
+
 }
 .title-header a{
   margin-left:5px;
   margin-right:5px;
 
-  padding:5px;
-  color:white;
+  padding:5px 10px 5px 10px; 
+color:#C13C2A;
   text-decoration: none;
+
+
+
+  background-color:snow;
+  border-radius:10px;
+  box-shadow:5px 5px 10px gray;
+
 
 
 }
@@ -72,8 +81,9 @@ h2{
 
 
 .title-header a:hover{
-  text-decoration: underline;
-  color:#FEDB32;
+
+  box-shadow:6px 6px 21px;
+
 }
 @media screen and (max-width: 370px){
 
